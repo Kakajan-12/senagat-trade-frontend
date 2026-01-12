6bea0062d0599386fc7493234b7da845b305322c
Kakajan-12 <83004880+Kakajan-12@users.noreply.github.com>
2026-01-12 20:41:56 +0500

    done

--- next.config.ts
+++ next.config.ts
@@ -2,27 +2,27 @@ import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-  // images: {
-  //     unoptimized: true,
-  //     remotePatterns: [
-  //         {
-  //             protocol: 'https',
-  //             hostname: 'api.oguzforum.com',
-  //             port: '',
-  //             pathname: '/uploads/**',
-  //         },
-  //     ],
-  // },
   images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        pathname: '/uploads/**'
-      },
-    ],
+      unoptimized: true,
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: 'api.senagat.com',
+              port: '',
+              pathname: '/uploads/**',
+          },
+      ],
   },
+  // images: {
+  //   unoptimized: true,
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'http',
+  //       hostname: 'localhost',
+  //       pathname: '/uploads/**'
+  //     },
+  //   ],
+  // },
 };
 
 const withNextIntl = createNextIntlPlugin();
